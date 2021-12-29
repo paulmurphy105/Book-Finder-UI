270459e7afb44ceb8a17e95023ec4752ba32e7cc
Paul Murphy <paulmurphy105@gmail.com>
2021-12-28 20:39:14 -0800

    feat: replace broken image with MUI icon

--- app/routes/components/NoBooksFound.jsx
+++ app/routes/components/NoBooksFound.jsx
@@ -1,12 +1,12 @@
 import Box from '@mui/material/Box';
 import { Typography } from "@mui/material";
+import SearchOff from '@mui/icons-material/SearchOff';
 
 export default function NoBooksFound() {
     return (
         <Box sx={{ display: 'flex', textAlign: 'center', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', margin: '2em' }}>
-            <Typography variant="h6" color="text.secondary" >No results found :( </Typography>
-            <img style={{ width: '200px', height: '200px' }} src="https://cdn-icons.flaticon.com/png/512/5326/premium/5326093.png?token=exp=1640746979~hmac=3c300ac1b4dfe2b9ba9836ec20e63eb8" alt="no books found logo" />
+            <Typography variant="h6" color="text.primary" >No results found</Typography>
+            <SearchOff sx={{ fontSize: 100, color: 'text.primary'}} />
         </Box>
     )
-
 }
