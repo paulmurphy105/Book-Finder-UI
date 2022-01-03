778e6e39db87301f8bd8e6208dcf8807f1badb09
Paul Murphy <paulmurphy105@gmail.com>
2022-01-03 10:57:58 -0800

    feat: silent fail when analytics call fails

--- app/utils/analytics.js
+++ app/utils/analytics.js
@@ -1,14 +1,20 @@
 const recordVisit = (request) => {
     const url = new URL(request.url) 
     
-    fetch(`${process.env.BACKEND_URL}visits`, {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ route: url.pathname, userAgent: request.headers.get('user-agent'), ipAddress: 'unknown', search: url.search })
-    });        
+    try {
+        fetch(`${process.env.BACKEND_URL}visits`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ route: url.pathname, userAgent: request.headers.get('user-agent'), ipAddress: 'unknown', search: url.search })
+        });  
+    } catch (error) {
+        console.error('Call to record visit failed')
+        console.error(error) 
+    }
+      
 }
 
 module.exports = {
